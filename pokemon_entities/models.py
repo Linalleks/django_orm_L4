bcc455eb038205797d07548b2d0191ac7a2c4d9f
Ангелина Петрова <linalleks@gmail.com>
2026-06-14 16:57:54 +0700

    add description pokemon

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(upload_to="images", null=True, blank=True)
+    description = models.TextField()
 
     def __str__(self):
         return self.title
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,9 +1,9 @@
 import folium
-import json
-import os
+# import json
+# import os
 
-from django.conf import settings
-from django.http import HttpResponseNotFound
+# from django.conf import settings
+# from django.http import HttpResponseNotFound
 from django.shortcuts import get_object_or_404, render
 from django.utils.timezone import now
 
@@ -58,32 +58,19 @@ def show_all_pokemons(request):
 
 
 def show_pokemon(request, pokemon_id):
-    with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
-        pokemons = json.load(database)['pokemons']
+    # with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
+    #     pokemons = json.load(database)['pokemons']
 
     pokemon = get_object_or_404(Pokemon, pk=pokemon_id)
-    # for pokemon in pokemons:
-    #     if pokemon['pokemon_id'] == int(pokemon_id):
-    #         requested_pokemon = pokemon
-    #         break
-    # else:
-    #     return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     for pokemon_entity in pokemon.entities.filter(appeared_at__lt=now(), disappeared_at__gt=now()):
         add_pokemon(
-            folium_map, 
+            folium_map,
             pokemon_entity.lat,
             pokemon_entity.lon,
             request.build_absolute_uri(pokemon_entity.pokemon.image.url)
         )
-    # for pokemon_entity in requested_pokemon['entities']:
-    #     add_pokemon(
-    #         folium_map,
-    #         pokemon_entity['lat'],
-    #         pokemon_entity['lon'],
-    #         pokemon['img_url']
-    #     )
 
     return render(request, 'pokemon.html', context={
         'map': folium_map._repr_html_(),
